8832fab8e5dfc5756188a96072b71b3b3cc2047c
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-06-23 16:25:03 +0500

    connected to PosgtreSQl

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.core.database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+    with engine.connect():
+        pass
+    
     return {"message": "StudyFlow API"}

0039a0c2b767157feddf5a54207dfa976593cb69
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-06-26 17:44:23 +0500

    adding schema User

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,1 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from app.enums.role import Role
+from datetime import date
+
+
+class UserCreate(BaseModel):
+    model_config = ConfigDict(
+        extra="forbid"
+    )
+
+    username: str
+    full_name: str
+    password: str
+    role: Role
+
+class UserRead(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+    id: int
+    username: str
+    full_name: str
+    role: Role
+    created_at: date
+
+class UserUpdate(BaseModel):
+    username: str | None = None
+    full_name: str | None = None
+    

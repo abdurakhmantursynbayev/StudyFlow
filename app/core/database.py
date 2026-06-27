77820dbdd1b5ade4a78f4d30c1bfbda453bfcdff
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-06-27 13:00:01 +0500

    finishing v1 schema

--- app/core/database.py
+++ app/core/database.py
@@ -1,4 +1,10 @@
 from sqlalchemy import create_engine
 from app.core.config import DATABASE_URL
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    expire_on_commit=False
+)
--- app/schemas/enrollment.py
+++ app/schemas/enrollment.py
@@ -1,3 +1,9 @@
 from pydantic import BaseModel, ConfigDict
 
 
+class EnrollmentCreate(BaseModel):
+    model_config = ConfigDict(
+        extra="forbid"
+    )
+
+    course_id: int

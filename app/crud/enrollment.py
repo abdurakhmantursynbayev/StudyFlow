43dce801ada8196c533024c4e6f85be0ead547db
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-06-28 13:35:16 +0500

    adding enrollment crud functions v2

--- app/crud/enrollment.py
+++ app/crud/enrollment.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from app.schemas.enrollment import EnrollmentCreate
 from app.models.enrollment import Enrollment
 from app.models.course import Course
+from sqlalchemy import select
+from app.models.user import User
 
 
 def enroll_student(db: Session, enrollment_data: EnrollmentCreate, student_id: int) -> Enrollment | None:
@@ -23,3 +25,18 @@ def enroll_student(db: Session, enrollment_data: EnrollmentCreate, student_id: i
     db.refresh(new_enrollment)
     return new_enrollment
 
+
+def get_enrollment(db: Session, student_id: int, course_id: int) -> Enrollment | None:
+    enrollment = db.get(Enrollment, (student_id, course_id))
+    return enrollment
+
+
+def get_student_courses(db: Session, student_id: int) -> list[Course]:
+    stmt = (
+        select(Course)
+        .join(Enrollment, Enrollment.course_id == Course.id)
+        .where(Enrollment.student_id == student_id)
+    )
+
+    data = db.execute(stmt).scalars().all()
+    return data

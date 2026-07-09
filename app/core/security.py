6ae4b86d35a0e01fa89ed63500e516767e4b0d02
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-07-09 15:29:31 +0500

    changing security.py

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,13 @@
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
+
+
+DUMMY_HASH = "$argon2id$v=19$m=65536,t=3,p=4$rKABGU5wFlznMfRK14jJpA$cbI2jLwKVQBE+KbtLhgREWqUj/UsXH+BGrAwxEOVkt0"
 
 
 def hash_password(password: str) -> str:
-    return password
+    return password_hash.hash(password)
+
+def password_verify(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)

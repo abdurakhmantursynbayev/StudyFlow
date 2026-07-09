bf8eb8f9338e0fe6b73a048198bdb8a9fde48e7e
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-07-09 17:21:42 +0500

    ...

--- app/core/config.py
+++ app/core/config.py
@@ -4,4 +4,6 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRES_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRES_MINUTES"))

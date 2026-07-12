5e2bc17551839b13e3f290f9041ebbeb07dcd2e4
AbdurakhmanTursynbayev <abdurakhmantursynbayev@gmail.com>
2026-07-12 22:18:16 +0500

    Create Homepage UI

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import catLogo from "../assets/cat-logo.png";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link> |{" "}
-            <Link to="/courses">Courses</Link> |{" "}
-            <Link to="/login">Login</Link> |{" "}
-            <Link to="/register">Register</Link>
+        <nav className="navbar">
+            <div className="logo">
+                <img src={catLogo} alt="StudyFlow Logo" />
+                <h2>StudyFlow</h2>
+            </div>
+
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/courses">Courses</Link>
+                <Link to="/login">Sign In</Link>
+                <Link to="/register">Register</Link>
+            </div>
         </nav>
     );
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-    return <h1>Home</h1>;
+    return (
+        <main className="hero">
+            <h1>StudyFlow</h1>
+
+            <p>
+                Learn. Build. Grow.
+            </p>
+
+            <span>
+                Modern learning platform powered by
+                <strong> FastAPI</strong>,
+                <strong> React</strong> and
+                <strong> PostgreSQL</strong>.
+            </span>
+
+            <Link to="/courses" className="hero-button">
+                Browse Courses
+            </Link>
+        </main>
+    );
 }
